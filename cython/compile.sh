c25b55461e55fbbb5ddbd904878748c9a056dafb
Philippe Dales <pdales@gmail.com>
2018-08-07 17:09:46 -0400

    nc

--- cython/compile.sh
+++ cython/compile.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export PYTHONPATH="${PYTHONPATH}:$../pyinclude/"
-export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:../include/
-
 rm *.so
-python3 setup.py build_ext --inplace
+python setup.py build_ext --inplace
 cp *.so ../pyinclude/xseis/.
